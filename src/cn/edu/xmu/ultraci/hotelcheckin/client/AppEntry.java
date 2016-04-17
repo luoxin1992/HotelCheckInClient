b5b5016234999ec8bb4829dfe69436d03568d3cd
luoxin1992 <646699784@qq.com>
2016-04-17 22:34:48 +0800

    建立工程

---
+++ src/cn/edu/xmu/ultraci/hotelcheckin/client/AppEntry.java
@@ -0,0 +1,16 @@
+package cn.edu.xmu.ultraci.hotelcheckin.client;
+
+import android.app.Application;
+
+/**
+ * 应用程序入口
+ * 
+ * @author LuoXin
+ *
+ */
+public class AppEntry extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
